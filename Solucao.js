e5ba8c8654b477fea74faf73e0987c157cb12164
amandaalvess <alvessamandasilva@gmail.com>
2017-05-11 10:21:18 -0300

    Update Solucao.js

--- Solucao.js
+++ Solucao.js
@@ -1,21 +1,23 @@
-var fs   = require('fs')
-var file = process.argv[2]
+var fs = require('fs');
+var file = process.argv[2];
 var extensao = file.split('.')[file.split('.').length-1]
-var numeros
+var number;
 
 function json()
 {
-	var numeros = require('./numeros.json');
-	numeros = numeros.numeros;
+	var number = require('./numeros.json');
+	number = number.numeros;
+  intervalos();
 }
 function csv()
 {
 	var csv = require('csv-string');
-	var resul;
+	var results;
 
 	fs.readFile(file, 'utf8', function(err, data)  {
-    resul = csv.parse(data);
-    numeros  = resul[0];
+    results = csv.parse(data);
+    number  = results[0];
+    intervalos();
 	});
 	
 }
@@ -26,7 +28,8 @@ function xml()
     fs.readFile(file, function(err, data) {
         xmlParser(data, function(err, result)
         	{
-        		numeros = result.numeros.split(',');
+        		number = result.number.split(',');
+            intervalos();
         	});
     });
     
@@ -37,27 +40,32 @@ if(extensao == 'csv')
 	csv();
 if(extensao == 'xml')
 	xml();
-
-numeros.sort(function(a, b){return a-b});
-var inicio = numeros[0];
+function intervalos(){
+number.sort(function(a, b){return a-b});
+var inicio = number[0];
 var fim = [];
-var resultado = "";
-for(var i = 0; i < numeros.length;  i++)
+var results = "";
+for(var i = 0; i < number.length;  i++)
 {
-	if(numeros[i + 1] != numeros[i] + 1)
-	{
-       		fim = numeros[i];
-       		if(fim == inicio)
-       		{
-       			resultado += "[" + inicio + "]";
-       		}
-       		else {
-       			resultado += "[" + inicio + "-" + fim + "] ";	
-       		}
-       		inicio = numeros[i+1];
-	}
+  if(number[i + 1] != number[i] + 1)
+  {
+          fim = number[i];
+          if(fim == inicio)
+          {
+            results += "[" + inicio + "]";
+          }
+          else {
+            results += "[" + inicio + "-" + fim + "] "; 
+          }
+          inicio = number[i+1];
+
+  }
 }
-fs.writeFile('resultado.json', JSON.stringify(resultado), function (err) {
-	if (err) return console.log(err);
-	console.log('Concluido');
+fs.writeFile('resultado.json', JSON.stringify(results
+  ), function (err) {
+  if (err) return console.log(err);
+  console.log(results);
 });
+
+}
+
